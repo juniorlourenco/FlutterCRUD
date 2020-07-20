fc1e26f6e60ff6c24d9ad94b5baa262689d8f4b7
Elizeo Lourenço da Silva Junior <juniorlourenco@Elizeos-iMac.local>
2020-07-20 00:59:58 -0400

    first commit

---
+++ lib/components/user_tile.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:FirstProject/models/user.dart';
+
+class UserTile extends StatelessWidget {
+  final User user;
+
+  const UserTile(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    final avatar = user.avatarUrl == null || user.avatarUrl.isEmpty
+        ? CircleAvatar(child: Icon(Icons.person))
+        : CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl));
+    return ListTile(
+      leading: avatar,
+    );
+  }
+}
---
+++ lib/views/user_list.dart
@@ -0,0 +1,20 @@
+import 'package:FirstProject/data/dummy_users.dart';
+import 'package:flutter/material.dart';
+import 'package:FirstProject/components/user_tile.dart';
+
+class UserList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final users = {...DUMMY_USERS};
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Usuários'),
+      ),
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+      ),
+    );
+  }
+}
