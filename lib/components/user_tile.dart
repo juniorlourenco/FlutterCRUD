ef5008611848526979178157f71d33dd89640205
juniorlourenco <juniorminaj101@gmail.com>
2020-07-20 14:07:32 -0400

    adding icons delete and edit

--- lib/components/user_tile.dart
+++ lib/components/user_tile.dart
@@ -12,7 +12,23 @@ class UserTile extends StatelessWidget {
         ? CircleAvatar(child: Icon(Icons.person))
         : CircleAvatar(backgroundImage: NetworkImage(user.avatarUrl));
     return ListTile(
-      leading: avatar,
-    );
+        leading: avatar,
+        title: Text(user.name),
+        subtitle: Text(user.email),
+        trailing: Container(
+          width: 100,
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.delete),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ));
   }
 }
