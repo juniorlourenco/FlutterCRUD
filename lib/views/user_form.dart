94b974e3879005d22e2dc1c0a27920a95c3a5913
Elizeo Lourenço da Silva Junior <juniorlourenco@Elizeos-iMac.local>
2020-07-26 20:37:18 -0400

    Including Form label texts

--- lib/views/user_form.dart
+++ lib/views/user_form.dart
@@ -6,6 +6,32 @@ class UserForm extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Formulário de Usuário'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Nome'),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'URL do Avatar'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
